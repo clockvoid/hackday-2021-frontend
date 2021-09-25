2ab95fc05426c7cdd61f55685b3ba4b6f99d1c27
clockvoid <hidenari.t1997nov@gmail.com>
2021-09-26 03:14:27 +0900

    apply: reactrouter

--- src/App.js
+++ src/App.js
@@ -4,64 +4,96 @@ import FileUploader from './FileUploader';
 import UploadProgress from './UploadProgress';
 import ResultList from './ResultList.js';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 
+const InitialMode = 0;
 const UploadMode = 1;
-const ViewInvalidMode = 2; // eslint-disable-line
+const RenderResultMode = 2;
 
 function App() {
 
   const [file, setFile] = useState(undefined);
   const [uploadProgress, setUploadProgress] = useState(0);
   const [results, setResults] = useState([]);
-  const [mode, setMode] = useState(UploadMode); // eslint-disable-line
+  const [mode, setMode] = useState(InitialMode);
 
   useEffect(() => {
-    if (mode === UploadMode) {
+    window.onpopstate = () => {
+      setMode(InitialMode);
+    };
+  });
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  useEffect(() => {
+    if (mode === InitialMode) {
       axios.request({
         method: 'get',
-        url: 'https://csvlinter.volare.site/'
+        url: 'https://opendatalinter.volare.site/'
       }).then(data => {
         setResults(data.data);
         console.log(data.data);
       });
+      setUploadProgress(0);
     }
   }, [mode]);
 
   useEffect(() => {
-    if (file === undefined || file == null) return;
+    if (file === undefined || file === null) return;
+
+    setMode(UploadMode);
 
     const submitData = new FormData();
     submitData.append("file", file);
     axios.request({
       method: 'post',
-      url: 'https://csvlinter.volare.site/',
+      url: 'https://opendatalinter.volare.site/',
       data: submitData,
       onUploadProgress: (e) => {
-        setUploadProgress(Math.floor(e.loaded / e.total * 100));
+        setUploadProgress(e.loaded / e.total * 100);
       }
     }).then(data => {
       console.log("Success: ", data);
-      setResults(data.data);
+      setMode(RenderResultMode);
+      sleep(2000).then(() => {
+        setResults(data.data);
+      });
     }).catch(error => {
       console.log("Error: ", error);
     });
   }, [file]);
 
   return (
-    <div class="App">
-      <header class="header">
-        <h1 class="headerInner">
-          Open Data Linter
-        </h1>
-      </header>
-      <main class="main">
-        <div class="mainInner">
-          <FileUploader setFile={setFile} />
-          <UploadProgress uploadProgress={uploadProgress} file={file} />
-          <ResultList results={results} />
-        </div>
-      </main>
-    </div>
+    <Router>
+      <div class="App">
+        <header class="header">
+          <h1 class="headerInner">
+            Open Data Linter
+          </h1>
+        </header>
+        <main class="main">
+          <div class="mainInner">
+            <Redirect to={mode === RenderResultMode ? "/result" : "/"} />
+            <Switch>
+              <Route path="/result">
+                <ResultList results={results} file={file} />
+              </Route>
+              <Route path="/">
+                { mode === InitialMode && <FileUploader setFile={setFile} /> }
+                { mode === UploadMode && <UploadProgress uploadProgress={uploadProgress} file={file} /> }
+              </Route>
+            </Switch>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
