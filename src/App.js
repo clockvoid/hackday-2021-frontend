2e8dbaf805546db1acadc35080bd84cd40dca478
clockvoid <hidenari.t1997nov@gmail.com>
2021-09-25 20:51:04 +0900

    fix: upload csv file when select file button is pressed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import FileUploader from './FileUploader';
 import UploadProgress from './UploadProgress';
 import ResultList from './ResultList.js';
+import axios from 'axios';
 
 function App() {
 
-  const [file, setFile] = useState();
+  const [file, setFile] = useState(undefined);
   const [uploadProgress, setUploadProgress] = useState(0);
-
-  const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  async function doProgress() {
-    await sleep(1000);
-
-    for (let i = 0; i < 100; i++) {
-      sleep(100).then(() => {
-        setUploadProgress(uploadProgress + 5);
-      });
-    }
-  }
-
-  doProgress();
 
   const results = [
     {name: "てすと", invalid_cells: ["1-1", "1-2"]},
     {name: "てすと2", invalid_cells: ["2-1", "2-2"]},
   ];
+
+  useEffect(() => {
+    if (file === undefined || file == null) return;
+
+    const submitData = new FormData();
+    submitData.append("file", file);
+    axios.request({
+      method: 'post',
+      url: 'https://csvlinter.volare.site/',
+      data: submitData,
+      onUploadProgress: (e) => {
+        setUploadProgress(e.loaded / e.total * 100);
+      }
+    }).then(data => {
+      console.log("Success: ", data);
+    }).catch(error => {
+      console.log("Error: ", error);
+    });
+  }, [file]);
 
   return (
     <div>
