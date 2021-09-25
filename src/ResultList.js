1cb01e05b325efbac3b858c79322fbdb6b1c9847
clockvoid <hidenari.t1997nov@gmail.com>
2021-09-25 23:23:59 +0900

    apply: style to real world result

--- src/ResultList.js
+++ src/ResultList.js
@@ -1,73 +1,98 @@
-import { useState } from 'react';
-
 const ResultList = (props) => {
   return (
     <div class="resultList">
       <h2 class="resultListHeadline">形式チェック</h2>
       <ul class="resultListListcontenter">
-        {props.results.map((result) =>
+        <>
+          {props.results.map((result) =>
           <ResultItem result={result} />
-        )}
+          )}
+        </>
       </ul>
     </div>
   );
 }
 
 const ResultItem = (props) => {
-  const [display, setDisplay] = useState("isOpen");
-  const [itemState, setItemState] = useState("isWaite")
 
-  const toggleDisplay = () => {
-    setDisplay(display === "isOpen" ? "isClose" : "isOpen");
-  }
-
+  const item = props.result.item;
+  const isValid = props.result.is_valid;
   const invalid_contents = props.result.invalid_contents[0];
+  const message = invalid_contents !== undefined ? invalid_contents.error_message : undefined;
   const cells = invalid_contents !== undefined ? invalid_contents.invalid_cells : undefined;
 
-  if (cells === undefined) {
+  if (isValid === true) {
+  // ok
     return (
-      <div>
-          <b>{props.result.item}</b>
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <li class="resultListListItem">
-        <div class="resultListListHeadline">
-          <span class="material-icons-outlined resultListListIcon resultListListIconWaiting">circle</span>
-          <p class="resultListListTitle">{props.name}</p>
-        </div>
-      </li>
       <li class="resultListListItem">
         <div class="resultListListHeadline">
           <span class="material-icons-outlined resultListListIcon resultListListIconChecked">check_circle</span>
-          <p class="resultListListTitle">{props.name}</p>
+          <p class="resultListListTitle">{item}</p>
         </div>
       </li>
+    );
+  } else if (isValid === false) { 
+  // だめなセルが存在
+    if (cells === undefined) {
+      return (
+        <li class="resultListListItem">
+          <div class="resultListListHeadline">
+            <span class="material-icons resultListListIcon resultListListIconError">block</span>
+              <p class="resultListListTitle">{item}</p>
+          </div>
+        </li>
+      );
+    }
+    if (message === undefined) {
+      return (
+        <li class="resultListListItem">
+          <div class="resultListListHeadline">
+            <span class="material-icons resultListListIcon resultListListIconError">block</span>
+            <p class="resultListListTitle">{item}</p>
+          </div>
+          <div class="resultListListContents">
+            {cells.map((cell) =>
+            <span class="resultListListContentsError">{cell[0]}行{cell[1]}列 ,</span>
+            )}
+          </div>
+        </li>
+      );
+    }
+    return (
+      <li class="resultListListItem">
+        <div class="resultListListHeadline">
+          <span class="material-icons resultListListIcon resultListListIconError">block</span>
+          <p class="resultListListTitle">{item}</p>
+        </div>
+        <div class="resultListListContents">
+          <p class="resultListListContentsErrorTitle">{message}</p>
+          {cells.map((cell) =>
+            <span class="resultListListContentsError">{cell[0]}行{cell[1]}列 ,</span>
+          )}
+        </div>
+      </li>
+    );
+  } else if (isValid === null) {
+    return (
       <li class="resultListListItem">
         <div class="resultListListHeadline">
           <span class="material-icons-outlined resultListListIcon resultListListIconBlocked">remove_circle_outline</span>
-          <p class="resultListListTitle">{props.name}</p>
+          <p class="resultListListTitle">{item}</p>
         </div>
         <div class="resultListListContents">
           <p class="resultListListContentsBlocked">ファイル形式が間違っているためチェックできません</p>
         </div>
       </li>
-      <li class="resultListListItem">
-        <div class="resultListListHeadline">
-          <span class="material-icons resultListListIcon resultListListIconError">block</span>
-          <p class="resultListListTitle">{props.name}</p>
-        </div>
-        <div class="resultListListContents">
-          <p class="resultListListContentsErrorTitle">固定文</p>
-          {props.cells.map((cell) =>
-            <span class="resultListListContentsError">{cell} ,</span>
-          )}
-        </div>
-      </li>
-    </>
+    );
+  };
+
+  return (
+    <li class="resultListListItem">
+      <div class="resultListListHeadline">
+        <span class="material-icons-outlined resultListListIcon resultListListIconWaiting">circle</span>
+        <p class="resultListListTitle">{item}</p>
+      </div>
+    </li>
   );
 }
 
