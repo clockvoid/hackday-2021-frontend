15b7f638148042178717ea97d2bebe2a9b815e5b
sp_DEGU <degudegu2510@gmail.com>
2021-09-25 18:19:58 +0900

    add fileuploader styles

--- src/FileUploader.js
+++ src/FileUploader.js
@@ -23,11 +23,12 @@ const FileUploader = (props) => {
   }
 
   return (
-    <div style={{height: 200, width: 300, borderStyle: "solid", borderColor: "#000000", borderWidth: 1}}>
+    <div class="fileUploader">
       <DragAndDrop handleDrop={handleDrop}>
-        <h2>ファイルをアップロードする</h2>
-        <p>CSV，Excel，PDFをドラッグ&ドロップして，形式をチェックします</p>
-        <button onClick={openFileSelector}>ファイルを選択</button>
+        <span class="material-icons fileUploaderIcon">cloud_upload</span>
+        <h2 class="fileUploaderHeadline">ファイルをアップロードする</h2>
+        <p class="fileUploadertext">CSV，Excel，PDFをドラッグ&ドロップして，形式をチェックします</p>
+        <button class="fileUploaderbutton" onClick={openFileSelector}>ファイルを選択</button>
       </DragAndDrop>
     </div>
   );
